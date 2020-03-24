8261b932b8c84a66983baddce06787ba5fbb4687
Apostolos Matsagkas <amatsagkas@gmail.com>
2020-03-24 17:57:35 +0200

    Expanded on the search example with result sizes, offset and snippet size

--- search_papers.py
+++ search_papers.py
@@ -7,22 +7,23 @@ INDEX_NAME = 'papers'
 
 
 # TODO: extract the field names used both here as well as when index-building to a common source of truth
-def search(es, query):
-    res = es.search(index=INDEX_NAME, body={"query": {"query_string": {
-        "query": query, "default_field": "fulltext",
-
-    }},
-        "highlight": {
-            "pre_tags": ["<b>"], "post_tags": ["</b>"],
-            "fields": {
-                "fulltext": {},
-                "abstract": {},
-                "authors": {},
-                "author_emails": {}
-            }
+def search(es, query, max_results=20, from_result=0, snippet_size=None):
+    query = {"query": {"query_string": {
+        "query": query, "default_field": "fulltext"}
+    }, "highlight": {
+        "pre_tags": ["<b>"], "post_tags": ["</b>"],
+        "fields": {
+            "fulltext": {},
+            "abstract": {},
+            "authors": {},
+            "author_emails": {}
         }
-    })
-    print('Got %d Hits:' % res['hits']['total']['value'])
+    }, 'size': max_results, 'from': from_result}
+    if snippet_size:
+        query['highlight']['fragment_size'] = snippet_size
+
+    print(query)
+    res = es.search(index=INDEX_NAME, body=query)
     # Here's your first hit
     for hit in res['hits']['hits'][:1]:
         print(hit['highlight'])
